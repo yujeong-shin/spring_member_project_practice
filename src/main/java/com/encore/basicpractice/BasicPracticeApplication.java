e6a2b4c0e52fb5b6f83002a19ce121e53060ddf3
yujeong-shin <vouobb@naver.com>
2024-01-20 18:23:45 +0900

    init

---
+++ src/main/java/com/encore/basicpractice/BasicPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.encore.basicpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encore/basicpractice/BasicPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encore.basicpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
